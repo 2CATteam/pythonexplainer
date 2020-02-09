8ce94ac7e3bc9f90b349eb47841fbfb8b253809a
Christian Hale <herochristmas@gmail.com>
2020-02-08 18:48:08 -0600

    Tet

--- Lowes.py
+++ Lowes.py
@@ -1,3 +1,15 @@
 def Lowes():
-    if 1 == 1:
-        print("True")
+    KeywordsPython = ["False", "class", "finally", "is", "return", "None", "continue", "for", "lambda", "try", "True", "def", "from", "nonlocal", "while", "and", "del", "global", "not", "with", "as", "elif", "if", "or", "yield", "assert", "else", "import", "pass", "break", "except", "in", "raise"]
+    KeywordsJava = ["abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while"]
+    KeywordsJS = ["abstract", "arguments", "await", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "debugger", "default", "delete", "do", "double", "else", "enum", "eval", "export", "extends", "false", "final", "finally", "float", "for", "function", "goto", "if", "implements", "import", "in", "instanceof", "int", "interface", "let", "long", "native", "new", "null", "package", "private", "protected", "public", "return", "short", "static", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "typeof", "var", "void", "volatile", "while", "with", "yield"]
+    KeywordsC = ["auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"]
+
+    InJava = [KeywordsJava[i] for i in range(len(KeywordsJava)) if KeywordsJava[i] not in KeywordsPython]
+    InJS = [KeywordsJS[i] for i in range(len(KeywordsJS)) if KeywordsJS[i] not in KeywordsPython]
+    InC = [KeywordsC[i] for i in range(len(KeywordsC)) if KeywordsC[i] not in KeywordsPython]
+    NotInPython = list(set(InJava) | set(InJS) | set(InC))
+
+    #print(NotInPython)
+
+
+Lowes()
